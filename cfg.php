93be1adc653a565eadbd7538e2b30f907df29f2b
Mr-J27 <108486380+Mr-J27@users.noreply.github.com>
2023-04-24 20:28:47 +0600

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,5 @@
+<?php
+
+$useragent = "Mozilla/5.0 (Linux; Android 9; RMX3085) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Mobile Safari/537.36";
+
+$cookie = "_ga=GA1.1.1943299826.1682283632; csrf_cookie_name=e1c8c9c64eb3767d45728701e470ff26; ci_session=3e1c0442ae9274429671a6c6c32ca9b711b88c32; _ga_40P1H2TSDD=GS1.1.1682346207.3.1.1682346211.0.0.0";
