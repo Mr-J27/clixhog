a4e4a063e3e6ef56b93384ff1ef08b795305581a
Mr-J27 <108486380+Mr-J27@users.noreply.github.com>
2023-04-24 20:28:15 +0600

    Delete cfg.php

--- cfg.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$useragent = "Mozilla/5.0 (Linux; Android 12; RMX3085) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Mobile Safari/537.36";
-
-$cookie = "ci_session=7a2d8e3d9b843408db2625ebc78d1ce080842d72; csrf_cookie_name=2dde4345f4c1e61c881d5ec38854798f; _ga=GA1.1.2028105679.1682191150; _ga_40P1H2TSDD=GS1.1.1682191149.1.1.1682191185.0.0.0";
